01baa1ae394ef4551a889a3b941fe1148ef4400f
naka <nakasyo2792@gmail.com>
2023-02-16 23:06:01 +0900

    [fix]homeの記事を修正しました。

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,67 +8,90 @@ import "./Card.css";
 const Card = () => {
     return (
     <div>
-        <Container>
-            <Row className="row article-card my-2">
-                <Col className="col-4 my-4">
-                    <Image src="../../public/logo512.png" className="article-img" />
-                </Col>
-                <Col className="list-article col-8 my-4">
-                    <p className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam perspiciatis aut ipsa necessitatibus possimus exercitationem aliquid ea assumenda ducimus?</p>
-                    <p>6分前</p>
-                    <p>株式会社</p>
-                </Col>
+        <Container className="my-4">
+            <Row className="row">
+                <Row className="col-6 article-card mx-2">
+                    <Col className="list-article col-8">
+                        <p className="title font-weight-bold">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p className="mt-2">6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
+                <Row className="col-6 article-card">
+                    <Col className="list-article col-8">
+                        <p className="title">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p>6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
+                <Row className="col-6 article-card mx-2">
+                    <Col className="list-article col-8">
+                        <p className="title font-weight-bold">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p className="mt-2">6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
+                <Row className="col-6 article-card">
+                    <Col className="list-article col-8">
+                        <p className="title">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p>6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
+                <Row className="col-6 article-card mx-2">
+                    <Col className="list-article col-8">
+                        <p className="title font-weight-bold">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p className="mt-2">6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
+                <Row className="col-6 article-card">
+                    <Col className="list-article col-8">
+                        <p className="title">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p>6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
+                <Row className="col-6 article-card mx-2">
+                    <Col className="list-article col-8">
+                        <p className="title font-weight-bold">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p className="mt-2">6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
+                <Row className="col-6 article-card">
+                    <Col className="list-article col-8">
+                        <p className="title">【岡山大学】文明動態学研究所 第1回特別展 『津島から世界へ、世界から津島へ』開会式典を行いました</p>
+                        <p>6分前</p>
+                        <p>株式会社</p>
+                    </Col>
+                    <Col className="col-4">
+                        <Image src="../../public/logo512.png" className="article-img" />
+                    </Col>
+                </Row>
             </Row>
-            <Row className="row article-card my-2">
-                <Col className="col-4 my-4">
-                    <Image src="../../public/logo512.png" className="article-img" />
-                </Col>
-                <Col className="list-article col-8 my-4">
-                    <p className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam perspiciatis aut ipsa necessitatibus possimus exercitationem aliquid ea assumenda ducimus?</p>
-                    <p>6分前</p>
-                    <p>株式会社</p>
-                </Col>
-            </Row>
-            <Row className="row article-card my-2">
-                <Col className="col-4 my-4">
-                    <Image src="../../public/logo512.png" className="article-img" />
-                </Col>
-                <Col className="list-article col-8 my-4">
-                    <p className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam perspiciatis aut ipsa necessitatibus possimus exercitationem aliquid ea assumenda ducimus?</p>
-                    <p>6分前</p>
-                    <p>株式会社</p>
-                </Col>
-            </Row>
-            <Row className="row article-card my-2">
-                <Col className="col-4 my-4">
-                    <Image src="../../public/logo512.png" className="article-img" />
-                </Col>
-                <Col className="list-article col-8 my-4">
-                    <p className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam perspiciatis aut ipsa necessitatibus possimus exercitationem aliquid ea assumenda ducimus?</p>
-                    <p>6分前</p>
-                    <p>株式会社</p>
-                </Col>
-            </Row>
-            <Row className="row article-card my-2">
-                <Col className="col-4 my-4">
-                    <Image src="../../public/logo512.png" className="article-img" />
-                </Col>
-                <Col className="list-article col-8 my-4">
-                    <p className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam perspiciatis aut ipsa necessitatibus possimus exercitationem aliquid ea assumenda ducimus?</p>
-                    <p>6分前</p>
-                    <p>株式会社</p>
-                </Col>
-            </Row>
-            <Row className="row article-card my-2">
-                <Col className="col-4 my-4">
-                    <Image src="../../public/logo512.png" className="article-img" />
-                </Col>
-                <Col className="list-article col-8 my-4">
-                    <p className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam perspiciatis aut ipsa necessitatibus possimus exercitationem aliquid ea assumenda ducimus?</p>
-                    <p>6分前</p>
-                    <p>株式会社</p>
-                </Col>
-            </Row>
+
         </Container>
     </div>
     );
