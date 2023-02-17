c3bf7d5a7aed40a271152a6bb7ddbe6729312252
amane <amachonchon@gmail.com>
2023-02-17 16:13:26 +0900

    Fix search tab

--- src/components/SearchTab.jsx
+++ src/components/SearchTab.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 import { Row } from 'react-bootstrap'
 import Prefectures from './Prefectures'
-import axios from 'axios'
 
 const SearchTab = ({cat, setConditions}) => {
     
@@ -11,17 +9,9 @@ const SearchTab = ({cat, setConditions}) => {
     const onStartDateChanged = (e) => setConditions((prevState) => ({ ...prevState, "startDate": e.target.value }))
     const onEndDateChanged = (e) => setConditions((prevState) => ({ ...prevState, "endDate": e.target.value }))
 
-    const onButtonClicked = () => {
-        // まだ何も
-    }
-
     return (
         <Form className="bg-warning p-3 mt-4">
             <Row className="justify-content-center">
-                {/* キーワード */}
-                <Form.Group className="mb-3">
-                    <Form.Control type="input" placeholder="キーワードで検索" />
-                </Form.Group>
                 {/* 都道府県 */}
                 <Form.Group className="mb-3">
                     <Form.Label htmlFor="Select">都道府県</Form.Label>
@@ -38,9 +28,6 @@ const SearchTab = ({cat, setConditions}) => {
                     <Form.Label htmlFor="disabledSelect">終了日</Form.Label>
                     <Form.Control type="date" placeholder="終了日" onChange={onEndDateChanged} />
                 </Form.Group>
-                <Button variant="primary" type="submit" onClick={onButtonClicked}>
-                    検索
-                </Button>
             </Row>
         </Form>
     )
