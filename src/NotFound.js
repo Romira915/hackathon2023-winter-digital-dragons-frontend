98c57eabebd596218e4e5a267887af1990e8b771
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-02-17 04:41:25 +0000

    test

--- src/NotFound.js
+++ src/NotFound.js
@@ -6,6 +6,7 @@ const NotFound = () => {
       <h1>お探しのページは見つかりませんでした．</h1>
       <div>
         <Link to={`/all/`}>ホームに戻る</Link>
+        <dev>{process.env.NODE_ENV}</dev>
       </div>
     </>
   );
